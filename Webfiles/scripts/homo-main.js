4234cabda2794b11300a0bdd154ec072b263b4a7
Tristan <tris10@my.yorku.ca>
2023-08-23 14:11:57 -0400

    Added transition for main, minor progress in js

--- Webfiles/scripts/homo-main.js
+++ Webfiles/scripts/homo-main.js
@@ -1,6 +1,12 @@
 /*
 Title: Questions js
 Date Created: 8/22/2023
-Last Edit: 8/22/2023
+Last Edit: 8/23/2023
 */
 
+let begin = document.getElementById("begin");
+
+begin.onclick = function () {
+    document.body.style.background = "grey";
+
+}
